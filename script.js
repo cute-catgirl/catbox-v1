ce345feb738dc9f295997aada465c584c901a2a0
Boxit379 <66697315+Boxit379@users.noreply.github.com>
2021-02-02 11:16:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var catpics = ["cat.png","cat1.png","cat2.png"]
-var randompic = Math.floor(Math.random() * catpics.length);
+var catpics = ["cat.png","cat1.png","cat2.png"];
+var randompic = 0;
 
 function openBox() {
+  randompic = Math.floor(Math.random() * catpics.length);
   document.getElementById('catbox').src=catpics[randompic];
   confetti.start();
 }
