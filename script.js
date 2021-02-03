4d380f5c461323f84ff2360242cb7d983aada2db
Ben <66697315+Boxit379@users.noreply.github.com>
2021-02-03 08:47:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ var randompic = 0;
 function openBox() {
   randompic = Math.floor(Math.random() * catpics.length);
   document.getElementById('catbox').src=catpics[randompic];
-  document.getElementById('catbox').setAttribute('onclick','')
+  document.getElementById('catbox').setAttribute('onclick','');
   confetti.start();
 }
 
 function closeBox() {
   document.getElementById('catbox').src="catbox.png";
+  document.getElementById('catbox').setAttribute('onclick','openBox()')
 }
