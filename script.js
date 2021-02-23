d3d87156a4b202f7a6e5db4da7e9d2e484f37d4f
Boxit379 <66697315+Boxit379@users.noreply.github.com>
2021-02-23 15:03:57 -0700

    sound effects

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var randompic = 0;
 function openBox() {
   randompic = Math.floor(Math.random() * catpics.length);
   document.getElementById('catbox').src=catpics[randompic];
-  document.getElementById('catbox').setAttribute('onclick','');
+  document.getElementById('catbox').setAttribute('onclick','playSound()');
   confetti.start();
   document.getElementById('bodyID').style.animation="backgroundAnim 5s linear 2s infinite normal";
 }
@@ -12,4 +12,9 @@ function openBox() {
 function closeBox() {
   document.getElementById('catbox').src="catbox.png";
   document.getElementById('catbox').setAttribute('onclick','openBox()')
+}
+
+function playSound() {
+  var audio = document.getElementById("meowSound");
+  audio.play();
 }
